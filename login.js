706b3e4d1e809118689a8f882d9d2aeb95bf2b7d
KCchronos <susk-c020418@student.jenseneducation.se>
2021-04-15 19:59:13 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,15 +1,26 @@
 
+
+/*Also note that localStorage can only store strings.
+
+To store arrays or objects, you would have to convert them to strings. */
+
+
 let userName = document.getElementById("userName");
 let passWord = document.getElementById("password");
 let person =[];
 let Email = [];
-let p = 0;
+let p = 1;
+
 function Store() {
-console.log(userName.value)
-person[p] = {email:userName.value, password: passWord.value};
-console.log(person)
+    let u =JSON.parse(localStorage.getItem('i'))
+    p =u.length;
+    console.log(u.length)
+    console.log(p)
+person[p]= {email:userName.value, password: passWord.value};
+
 localStorage.setItem('i', JSON.stringify(person));
-p++;
+
+
 }
 
 
@@ -30,30 +41,45 @@ function check() {
     return false;
    
 };
-/*Also note that localStorage can only store strings.
-
-To store arrays or objects, you would have to convert them to strings. */
 
 
 
-/*let userName = document.getElementById("userName");
+
+
+
+
+/*
+let userName = document.getElementById("userName");
 let passWord = document.getElementById("password");
+let person =[];
+let Email = [];
 
+//https://stackoverflow.com/questions/12162786/adding-new-objects-to-localstorage
 function Store() {
-localStorage.setItem("user", userName.value);
-localStorage.setItem("pw", passWord.value);
+    let oldItems = JSON.parse(localStorage.getItem('i')) || [];
+console.log(userName.value)
+person= {email:userName.value, password: passWord.value};
+oldItems.push(person)
+console.log(person)
+localStorage.setItem('i', JSON.stringify(oldItems));
+
 }
-let x = localStorage.getItem('user');
-let y = localStorage.getItem('pw');
+
 
 function check() {
-    
-    if( x === userName.value && y === passWord.value) {
-    alert("You are logged in")
-    }
+    let Email=JSON.parse(localStorage.getItem('i'));
+    console.log (Email[1].email);
+    for (let p = 0; p < Email.length; p++) {
+        
+        if( userName.value === Email[p].email&& passWord.value === Email[p].password) {
 
-    else {
-        alert("you frogot your password idiot")
-    }
-}
-*/
+            alert("You are logged in")
+            return true;   
+        }
+            
+    }  
+            
+    alert('you forgot your password');
+    return false;
+   
+};*/
